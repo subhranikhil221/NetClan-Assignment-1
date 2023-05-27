126d3f3030e11d36563b2207ddf3918f9d546056
Harshvardhan <jitug4204@gmail.com>
2023-05-27 10:49:22 +0530

    Refine layout added

--- app/src/main/java/com/example/sample/MainActivity.java
+++ app/src/main/java/com/example/sample/MainActivity.java
@@ -1,23 +1,26 @@
 package com.example.sample;
 
-import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
     ImageView refine;
     TabLayout tabLayout;
     ViewPager viewPager;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
         refine = findViewById(R.id.refine);
         tabLayout = findViewById(R.id.tabLayout);
         viewPager = findViewById(R.id.viewPager);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
+        toolbar = findViewById(R.id.toolbar);
+
+        ActionBarDrawerToggle abd = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_open,R.string.navigation_close);
+
+        drawerLayout.addDrawerListener(abd);
+        abd.syncState();
 
         refine.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/sample/Refine.java
+++ app/src/main/java/com/example/sample/Refine.java
@@ -1,14 +1,30 @@
 package com.example.sample;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class Refine extends AppCompatActivity {
 
+    String[] arrAvailability = {
+            "Available | Hey Let Us Connect",
+            "Away | Stay Discreet And Watch",
+            "Busy | Do not Disturb | Will Catch Up Later",
+            "SOS | Emergency! Need Assistance! HELP!"
+    };
+    AutoCompleteTextView autoComplete1;
+    AutoCompleteTextView autoComplete2;
+    ArrayAdapter<String> adapterItems;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_refine);
+
+        autoComplete1 = findViewById(R.id.autoComplete1);
+//        autoComplete2 = findViewById(R.id.autoComplete2);
+        adapterItems = new ArrayAdapter<>(this,R.layout.list_availability,arrAvailability);
+
+        autoComplete1.setAdapter(adapterItems);
     }
 }
